7728ef412d503be8b9a63add149bd53dedcf80ec
thiagoc789 <santiago.cruz@correounivalle.edu.co>
2022-07-21 01:07:50 -0500

    Eventos

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views.userViews import UserView
+from api.views.eventosViews import EventoViewSet
 
 urlpatterns = [
+    path('eventos/', EventoViewSet.as_view(), name='eventos_list'),
     path('users/', UserView.as_view(), name='users_list'),
     path('users/<int:id>', UserView.as_view(), name='user_info'),
 ]
